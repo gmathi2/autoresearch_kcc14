5de2014c7019e5c70921124aabf092c7e728c3e6
gmathi2 <georg.mathi@gmail.com>
2026-03-26 22:17:21 +0100

    baseline from mar26_v2 (64491.0 cost)

--- input/KCC14/src/com/knapp/codingcontest/solution/Solution.java
+++ input/KCC14/src/com/knapp/codingcontest/solution/Solution.java
@@ -14,167 +14,262 @@
 
 package com.knapp.codingcontest.solution;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import java.util.logging.Level;
+import java.util.Map;
+import java.util.Set;
 
 import com.knapp.codingcontest.data.Container;
 import com.knapp.codingcontest.data.Institute;
+import com.knapp.codingcontest.data.Location;
 import com.knapp.codingcontest.data.Order;
 import com.knapp.codingcontest.data.Rack;
 import com.knapp.codingcontest.data.Waypoint;
 import com.knapp.codingcontest.operations.AeroBot;
-import com.knapp.codingcontest.operations.AeroBotVisualizer;
-import com.knapp.codingcontest.operations.ChargingArea;
-import com.knapp.codingcontest.operations.InfoSnapshot;
-import com.knapp.codingcontest.operations.ParkingArea;
-import com.knapp.codingcontest.operations.PickArea;
 import com.knapp.codingcontest.operations.Warehouse;
 
-/**
- * This is the code YOU have to provide
- */
 public class Solution {
   public String getParticipantName() {
-    return null; // TODO: return your name
+    return "AutoResearch";
   }
 
   public Institute getParticipantInstitution() {
-    return null; // TODO: return the Id of your institute - please refer to the hand-out
+    return Institute.Sonstige;
   }
-
-  // ----------------------------------------------------------------------------
 
   protected final Warehouse warehouse;
-
-  // ----------------------------------------------------------------------------
+  
+  private final Map<AeroBot, Integer> botAssignedOrder = new HashMap<>();
+  private final Map<AeroBot, String> botAssignedContainer = new HashMap<>();
+  private final Map<AeroBot, Rack.RackStorageLocation> botContainerHome = new HashMap<>();
+  private final Set<String> currentTurnStoreReservationCodes = new HashSet<>();
 
   public Solution(final Warehouse warehouse) {
-    // TODO: prepare data structures (may also be done in run() method below)
     this.warehouse = warehouse;
-    if (getParticipantName() == null) {
-      throw new IllegalArgumentException("let getParticipantName() return your name");
-    }
-    if (getParticipantInstitution() == null) {
-      throw new IllegalArgumentException("let getParticipantInstitution() return yout institution");
-    }
   }
 
-  // ----------------------------------------------------------------------------
-
-  /**
-   * The main entry-point.
-   *
-   */
   public void run() throws Exception {
-    // TODO: make calls to API (see below)
+    while (!warehouse.areAllOrdersFinished()) {
+      assignTasks();
+      warehouse.executeTicksUntilFirstBotToFinish();
+      
+      if (allBotsIdle() && !warehouse.areAllOrdersFinished()) {
+          warehouse.executeOneTick();
+      }
+    }
   }
 
-  // ----------------------------------------------------------------------------
-  // ----------------------------------------------------------------------------
-
-  /**
-   * Just for documentation purposes.
-   *
-   * this method may be removed without any side-effects
-   *   divided into these sections
-   *
-   *     <li><em>input methods</em>
-   *
-   *     <li><em>main interaction methods</em>
-   *         - these methods are the ones that make (explicit) changes to the warehouse
-   *
-   *     <li><em>information</em>
-   *         - information you might need for your solution
-   *
-   *     <li><em>additional information</em>
-   *         - various other infos: statistics, information about (current) costs, ...
-   *
-   */
-  @SuppressWarnings({ "unused", "null" })
-  private void apis() throws Exception {
-    final AeroBot aeroBot = null;
-    final Waypoint waypoint = null;
-    final int level = 0;
-    final Container container = null;
-    final Order order = null;
-
-    // ----- input -----
-
-    final Collection<Container> ac = warehouse.getAllContainers();
-    final List<Order> ao = warehouse.getAllOrders();
-    final Collection<Rack> ar = warehouse.getAllRacks();
-
-    // ----- main interaction methods -----
-
-    aeroBot.planMoveToWaypoint(waypoint);
-    aeroBot.planClimbToLevel(level);
-    aeroBot.planLoadContainer(container);
-    aeroBot.planPick(order);
-    aeroBot.planStoreContainer();
-    aeroBot.planStartCharge();
-
-    // ----- information -----
-
-    final ParkingArea parking = warehouse.getParkingArea();
-    final ChargingArea charging = warehouse.getChargingArea();
-    final PickArea picking = warehouse.getPickArea();
-    final Collection<AeroBot> aeroBots = warehouse.getAllAeroBots();
-
-    final Collection<Container> acs = warehouse.findAvailableContainers("productCode");
-    final Collection<Rack.RackStorageLocation> ersls = warehouse.findEmptyRackStorageLocations();
-
-    final List<Order> openOrders = warehouse.getOpenOrders();
-    final boolean allFinished = warehouse.areAllOrdersFinished();
-
-    // ----- additional information -----
-
-    final Collection<AeroBot> pabs = parking.getParkingAeroBots();
-
-    final int cs = charging.getChargingSlots();
-    final int fcs = charging.getFreeChargingSlots();
-    charging.hasFreeChargingSlot();
-
-    final List<Order> porders = picking.getCurrentOrders();
-
-    final InfoSnapshot info = warehouse.getInfoSnapshot();
-
-    //
-    final int uo = info.getUnfinishedOrderCount();
-    final int t = info.getTicksRuntime();
-
-    // Park, MoveH, MoveV, Load, Pick, Store, Charge, _Idle_
-    final InfoSnapshot.AeroBotStatistics abstat = info.getAeroBotStatistics(aeroBot);
-    final InfoSnapshot.AeroBotStatistics.ModeStatistics abms = abstat.getModeStatistics(AeroBot.OperationMode._Idle_);
-    final InfoSnapshot.AeroBotStatistics.ModeStatistics ms = info.getModeStatistics(AeroBot.OperationMode._Idle_);
-    ms.get_count();
-    ms.get_tick_units();
-    ms.get_ticks();
-
-    //
-    final double c_uo = info.getUnfinishedOrdersCost();
-    final double c_t = info.getTicksCost();
-    final double c_T = info.getTotalCost();
-
-    // optional logging (SEVERE, WARNING, INFO, CONFIG, FINE, FINER, FINEST)
-    // if used you may adjust some attributes below ...
-    warehouse.log(Level.FINEST, "finest");
-
-    final AeroBotVisualizer v = warehouse.getVisualizer();
-    final String filename = null;
-    v.generateHTML(filename);
-    v.getConflictCount();
-    v.getFirstConflictTick();
+  private boolean allBotsIdle() {
+    for (AeroBot bot : warehouse.getAllAeroBots()) {
+      if (!bot.getOpenOperations().isEmpty()) {
+        return false;
+      }
+    }
+    return true;
   }
 
-  static {
-    // TODO: you may change logging (target: file or console/stderr, level) - you may turn it 'OFF'
-    //System.setProperty("initLogging", "solution.log"); // default: 'console' (stdout); use file instead of console
-    //System.setProperty("logTarget", "stderr"); // default: stdout (only considered with initLogging=console)
-    //System.setProperty("logLevel", "FINE"); // default: INFO (OFF, SEVERE, WARNING, INFO, CONFIG, FINE, FINER, FINEST, ALL)
+  private void assignTasks() {
+    currentTurnStoreReservationCodes.clear();
+    // 1. Identify idle bots and candidates
+    List<AeroBot> idleBots = new ArrayList<>();
+    for (AeroBot bot : warehouse.getAllAeroBots()) {
+        if (bot.getOpenOperations().isEmpty()) {
+            botAssignedOrder.remove(bot);
+            idleBots.add(bot);
+        }
+    }
 
-    // System.setProperty("useVisualizer", "false"); // uncomment to disable data-collection for visualization
+    if (idleBots.isEmpty()) return;
+
+    Set<Integer> currentlyAssigned = new HashSet<>(botAssignedOrder.values());
+    List<Order> candidates = getCandidates(currentlyAssigned);
+
+    // 2. Separate bots with containers and without
+    List<AeroBot> botsWithContainer = new ArrayList<>();
+    List<AeroBot> botsWithoutContainer = new ArrayList<>();
+    for (AeroBot bot : idleBots) {
+        if (bot.getCurrentCharge() < bot.getMaxCharge() * 0.3) {
+            if (bot.getCurrentContainer() != null) {
+                planStore(bot);
+            } else {
+                bot.planMoveToWaypoint(warehouse.getChargingArea());
+                bot.planStartCharge();
+            }
+            continue;
+        }
+
+        if (bot.getCurrentContainer() != null) {
+            botsWithContainer.add(bot);
+        } else {
+            botsWithoutContainer.add(bot);
+        }
+    }
+
+    // 3. Match bots with containers FIRST (reuse)
+    for (AeroBot bot : botsWithContainer) {
+        Container current = bot.getCurrentContainer();
+        Order matched = null;
+        for (Order o : candidates) {
+            if (o.getProductCode().equals(current.getProductCode()) && !currentlyAssigned.contains(o.getSequence())) {
+                matched = o;
+                break;
+            }
+        }
+        if (matched != null) {
+            botAssignedOrder.put(bot, matched.getSequence());
+            currentlyAssigned.add(matched.getSequence());
+            bot.planPick(matched);
+        } else if (!shouldWait(current.getProductCode())) {
+            planStore(bot);
+        }
+    }
+
+    // 4. Sequential greedy matching for bots without containers
+    List<Order> unassigned = new ArrayList<>();
+    for (Order o : candidates) {
+        if (!currentlyAssigned.contains(o.getSequence())) {
+            unassigned.add(o);
+        }
+    }
+
+    while (!botsWithoutContainer.isEmpty() && !unassigned.isEmpty()) {
+        Order order = unassigned.get(0);
+        AeroBot bestBot = null;
+        Rack.RackStorageLocation bestRsl = null;
+        Container bestContainer = null;
+        int minCharge = Integer.MAX_VALUE;
+
+        for (AeroBot bot : botsWithoutContainer) {
+            Assignment bestForPair = findBestContainerForPair(bot, order);
+            if (bestForPair != null && bestForPair.charge < minCharge) {
+                minCharge = bestForPair.charge;
+                bestBot = bot;
+                bestRsl = bestForPair.rsl;
+                bestContainer = bestForPair.container;
+            }
+        }
+
+        if (bestBot != null) {
+            if (bestBot.getCurrentCharge() >= minCharge + 1000) {
+                botAssignedOrder.put(bestBot, order.getSequence());
+                botAssignedContainer.put(bestBot, bestContainer.getCode());
+                botContainerHome.put(bestBot, bestRsl);
+                currentlyAssigned.add(order.getSequence());
+
+                bestBot.planMoveToWaypoint(bestRsl.getWaypoint());
+                bestBot.planClimbToLevel(bestRsl.getLevel());
+                bestBot.planLoadContainer(bestContainer);
+                bestBot.planClimbToLevel(0);
+                bestBot.planMoveToWaypoint(warehouse.getPickArea());
+                bestBot.planPick(order);
+            }
+            botsWithoutContainer.remove(bestBot);
+        }
+        unassigned.remove(0);
+    }
   }
 
-  // ----------------------------------------------------------------------------
+  private static class Assignment {
+      Rack.RackStorageLocation rsl;
+      Container container;
+      int charge;
+  }
+
+  private Assignment findBestContainerForPair(AeroBot bot, Order order) {
+      Collection<Container> containers = warehouse.findAvailableContainers(order.getProductCode());
+      Assignment best = null;
+      int minCharge = Integer.MAX_VALUE;
+
+      for (Container container : containers) {
+          if (botAssignedContainer.containsValue(container.getCode())) continue;
+
+          Location loc = container.getCurrentLocation();
+          if (loc != null && loc.getType() == Location.Type.Rack) {
+              Rack.RackStorageLocation rsl = (Rack.RackStorageLocation) loc;
+              int charge = estimateFetchPickCharge(bot, rsl, order, container);
+              if (charge < minCharge) {
+                  minCharge = charge;
+                  if (best == null) best = new Assignment();
+                  best.rsl = rsl;
+                  best.container = container;
+                  best.charge = charge;
+              }
+          }
+      }
+      return best;
+  }
+
+  private List<Order> getCandidates(Set<Integer> currentlyAssigned) {
+      List<Order> candidates = new ArrayList<>(warehouse.getPickArea().getCurrentOrders());
+      List<Order> open = warehouse.getOpenOrders();
+      for (Order o : open) {
+          if (candidates.size() >= 50) break;
+          boolean already = false;
+          for (Order c : candidates) {
+              if (c.getSequence().equals(o.getSequence())) {
+                  already = true;
+                  break;
+              }
+          }
+          if (!already) candidates.add(o);
+      }
+      return candidates;
+  }
+
+  private void planStore(AeroBot bot) {
+    Collection<Rack.RackStorageLocation> empty = warehouse.findEmptyRackStorageLocations();
+    Rack.RackStorageLocation best = null;
+    int bestScore = Integer.MAX_VALUE;
+
+    for (Rack.RackStorageLocation rsl : empty) {
+        if (currentTurnStoreReservationCodes.contains(rsl.getCode()) || warehouse.isReserved(rsl)) {
+            continue;
+        }
+        int s = scoreLocation(rsl);
+        if (s < bestScore) {
+            bestScore = s;
+            best = rsl;
+        }
+    }
+
+    if (best != null) {
+        currentTurnStoreReservationCodes.add(best.getCode());
+        bot.planMoveToWaypoint(best.getWaypoint());
+        bot.planClimbToLevel(best.getLevel());
+        bot.planStoreContainer();
+        bot.planClimbToLevel(0);
+    }
+    botContainerHome.remove(bot);
+    botAssignedContainer.remove(bot);
+  }
+
+  private int scoreLocation(Rack.RackStorageLocation rsl) {
+      if (rsl == null) return Integer.MAX_VALUE;
+      int dist = rsl.getWaypoint().distance(warehouse.getPickArea());
+      return dist * 3 + rsl.getLevel() * 15;
+  }
+
+  private boolean shouldWait(String productCode) {
+    List<Order> open = warehouse.getOpenOrders();
+    int limit = Math.min(open.size(), 30); // only wait if needed very soon
+    for (int i = 0; i < limit; i++) {
+        if (open.get(i).getProductCode().equals(productCode)) return true;
+    }
+    return false;
+  }
+
+  private int estimateFetchPickCharge(AeroBot bot, Rack.RackStorageLocation rsl, Order order, Container container) {
+    int charge = 0;
+    Waypoint botPos = bot.getCurrentWaypoint();
+    charge += warehouse.calculateMoveToWaypoint(botPos, rsl.getWaypoint()).charge;
+    charge += warehouse.calculateClimbToLevel(0, rsl.getLevel()).charge;
+    charge += warehouse.calculateLoadContainer(container).charge;
+    charge += warehouse.calculateClimbToLevel(rsl.getLevel(), 0).charge;
+    charge += warehouse.calculateMoveToWaypoint(rsl.getWaypoint(), warehouse.getPickArea()).charge;
+    charge += warehouse.calculatePick(order).charge;
+    return charge;
+  }
 }
